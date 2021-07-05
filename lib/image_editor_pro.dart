6e2c53dbeb2c13b81525806718701c0120f07fd8
HungND <hungnd@newwave.vn>
2021-07-05 17:06:04 +0700

    fix lib

--- lib/image_editor_pro.dart
+++ lib/image_editor_pro.dart
@@ -433,7 +433,7 @@ class _ImageEditorProState extends State<ImageEditorPro> {
               child: TextFormField(
                 controller: inputTextController,
                 decoration: InputDecoration(
-                  contentPadding: EdgeInsets.only(top: 5, left: 2),
+                  contentPadding: EdgeInsets.only(top: 5),
                   border: InputBorder.none,
                 ),
                 onChanged: (value) {
